5e00e17437fb90691a007412fad5cd7064a68795
sp41mer <sp41mer@yahoo.com>
2016-08-10 21:48:43 +0300

    normal create script

--- Postgres_only/connections.py
+++ Postgres_only/connections.py
@@ -3,17 +3,19 @@ from models import *
 import psycopg2
 
 def create_tables():
+    #create tables for peewee
     database.connect()
     database.create_tables([Group])
+    #create tables for psycopg2
+    conn_string = "host='localhost' dbname='only_postgres_parcing' user='root' password='root'"
+    conn = psycopg2.connect(conn_string)
+    cur = conn.cursor()
+    cur.execute("create table users_and_friends (id SERIAL PRIMARY KEY, vk_id INT, friends INT[]);"
+                "create table groups_and_users (id SERIAL PRIMARY KEY, vk_id INT, users INT[]);")
+    conn.commit()
+    cur.close()
+    conn.close()
 
-# conn_string = "host='localhost' dbname='only_postgres_parcing' user='root' password='root'"
-# conn = psycopg2.connect(conn_string)
-# cur = conn.cursor()
-# cur.execute("create table users_and_friends (id SERIAL PRIMARY KEY, vk_id INT, friends INT[]);"
-#             "create table groups_and_users (id SERIAL PRIMARY KEY, vk_id INT, users INT[]);")
-# conn.commit()
-# cur.close()
-# conn.close()
 create_tables()
 
 
