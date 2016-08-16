a9add2553ba018f2dd1453d42dbf3eea27d0d311
sp41mer <sp41mer@yahoo.com>
2016-08-16 19:05:29 +0300

    production version

--- Postgres_and_Cassandra/search_grabber.py
+++ Postgres_and_Cassandra/search_grabber.py
@@ -14,19 +14,18 @@ def get_groups(query):
                                       'v': 5.53,
                                       'access_token': vk_access_token})
     data = json.loads(response.text)
-    print data
-    # group_list = data['response']['items']
-    # for group in group_list:
-    #     print group.get('name', '')
-    #     new_group = Group(
-    #         vk_id=group.get('id', ''),
-    #         query_string= search_query,
-    #         name=group.get('name', ''),
-    #         screen_name=group.get('screen_name', ''),
-    #         type=group.get('type', ''),
-    #         photo_50=group.get('photo_50', ''),
-    #         photo_100=group.get('photo_100', ''),
-    #         photo_200=group.get('photo_200', '')
-    #     )
-    #     new_group.save()
-    #     # time.sleep(0.4)
+    group_list = data['response']['items']
+    for group in group_list:
+        print group.get('name', '')
+        new_group = Group(
+            vk_id=group.get('id', ''),
+            query_string= search_query,
+            name=group.get('name', ''),
+            screen_name=group.get('screen_name', ''),
+            type=group.get('type', ''),
+            photo_50=group.get('photo_50', ''),
+            photo_100=group.get('photo_100', ''),
+            photo_200=group.get('photo_200', '')
+        )
+        new_group.save()
+        # time.sleep(0.4)
