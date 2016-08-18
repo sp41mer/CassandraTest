25ccd1c8817e52f8d264d546c7eb4f47af98eba0
sp41mer <sp41mer@yahoo.com>
2016-08-18 15:01:45 +0300

    without orm first step

--- WithoutORM/main.py
+++ WithoutORM/main.py
@@ -1,20 +1,16 @@
 __author__ = 'sp41mer'
-from cassandra.cqlengine import connection
-from cassandra.cluster import Cluster
-from cassandra.cqlengine.management import sync_table
-from cassandra.auth import PlainTextAuthProvider
+# from cassandra.cqlengine import connection
+# from cassandra.cluster import Cluster
+# from cassandra.cqlengine.management import sync_table
+# from cassandra.auth import PlainTextAuthProvider
 
+#
+# cluster = Cluster(['127.0.0.1'])
+# session = cluster.connect()
+# session.set_keyspace('parse_keyspace')
 
-cluster = Cluster(['127.0.0.1'])
-session = cluster.connect()
-session.set_keyspace('parse_keyspace')
-session.execute("CREATE TABLE users (id int PRIMARY KEY, location text)")
-# session = connection.setup(['127.0.0.1'], "parse_keyspace", protocol_version=3)
-# session.execute("CREATE TABLE IF NOT EXISTS simplex.songs (" +
-#             "id uuid PRIMARY KEY," +
-#             "title text," +
-#             "album text," +
-#             "artist text," +
-#             "tags set<text>," +
-#             "data blob" +
-#             ");")
+string_of_fields = 'friend1 int PRIMARY KEY, '
+for i in range(2,11):
+    string_of_fields += 'friend'+str(i) +' int,'
+
+print string_of_fields[:-1]
