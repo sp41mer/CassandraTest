88450ae08ec6ff33064f1007223c1e7949c9d924
sp41mer <sp41mer@yahoo.com>
2016-08-18 15:10:57 +0300

    without orm second  step

--- WithoutORM/main.py
+++ WithoutORM/main.py
@@ -1,16 +1,17 @@
 __author__ = 'sp41mer'
-# from cassandra.cqlengine import connection
-# from cassandra.cluster import Cluster
-# from cassandra.cqlengine.management import sync_table
-# from cassandra.auth import PlainTextAuthProvider
+from cassandra.cqlengine import connection
+from cassandra.cluster import Cluster
+from cassandra.cqlengine.management import sync_table
+from cassandra.auth import PlainTextAuthProvider
 
-#
-# cluster = Cluster(['127.0.0.1'])
-# session = cluster.connect()
-# session.set_keyspace('parse_keyspace')
 
-string_of_fields = 'friend1 int PRIMARY KEY, '
-for i in range(2,11):
+userid = 123456
+cluster = Cluster(['127.0.0.1'])
+session = cluster.connect()
+session.set_keyspace('parse_keyspace')
+
+string_of_fields = 'pk uuid PRIMARY KEY, '
+for i in range(1,11):
     string_of_fields += 'friend'+str(i) +' int,'
 
-print string_of_fields[:-1]
+session.execute("CREATE TABLE "+str(userid)+ "(" +string_of_fields+")")
